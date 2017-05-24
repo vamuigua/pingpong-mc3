02c9087544570bf93e3e362d1422d8e27fd59a97
Victor Allen <vamuigua@gmail.com>
2017-05-24 15:32:14 +0300

    Updated content

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "711755f031016df07047c82abc0251fb";
+
+},{}],2:[function(require,module,exports){
 function Calculator(skinName) {
   this.skin = skinName;
 }
@@ -19,7 +22,23 @@ Calculator.prototype.pingPong = function(goal) {
 };
 exports.calculatorModule = Calculator;
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
+var apiKey = require('./../.env').apiKey;
+
+function Weather(){
+}
+
+Weather.prototype.getWeather = function(city) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+  }).fail(function(error) {
+    $('.showWeather').text(error.responseJSON.message);
+  });
+}
+
+exports.weatherModule = Weather;
+
+},{"./../.env":1}],4:[function(require,module,exports){
 var Calculator = require('./../js/pingpong.js').calculatorModule;
 $(document).ready(function() {
   $('#ping-pong-form').submit(function(event) {
@@ -46,4 +65,19 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
-},{"./../js/pingpong.js":1}]},{},[2]);
+var apiKey = require('./../.env').apiKey;
+var Weather = require('./../js/weather.js').weatherModule;
+
+//The then() method of a promise is called when a promise enters the fulfilled state
+//The fail() method is called when a promise enters the rejected state
+
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    currentWeatherObject.getWeather(city);
+  });
+});
+
+},{"./../.env":1,"./../js/pingpong.js":2,"./../js/weather.js":3}]},{},[4]);
